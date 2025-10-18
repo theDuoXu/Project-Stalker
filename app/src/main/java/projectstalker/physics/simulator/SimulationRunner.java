6db5fe5b39d230932fc9457c3a375754faf263b2
duo <duo@pemail.es>
2025-10-19 01:13:52 +0200

    Coordinador de simulaciones, coordina entre manning y transport

---
+++ app/src/main/java/projectstalker/physics/simulator/SimulationRunner.java
@@ -0,0 +1,11 @@
+package projectstalker.physics.simulator;
+
+/**
+ * Clase orquestadora de simulaciones.
+ * Orquesta una simulación de Manning para calcular el cuerpo de agua inicial (profundidades y velocidades)
+ * A continuación orquesta la simulación del Transporte de contaminantes en el río sobre los ríos iniciales
+ * TODO No existe el simulador de transporte, no se ha implementado su calculador
+ */
+public class SimulationRunner {
+
+}
