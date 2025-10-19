d029d83fb96665f1271f9194aa48880cdaa12d6e
duo <duo@pemail.es>
2025-10-19 17:33:27 +0200

    Forzamos al compilador de C++ a buscar los nombres C (sin mutilar). Resuelve error en el test gpu

--- native-lib/src/main/cpp/src/physics/manning_solver.cpp
+++ native-lib/src/main/cpp/src/physics/manning_solver.cpp
@@ -1,7 +1,11 @@
 // manning_solver.cpp
 
 #include "projectstalker/physics/manning_solver.h"
-#include "projectstalker/physics/manning_kernel.h" // Nuestro lanzador del Paso 1
+// Forzamos al compilador de C++ a buscar los nombres C (sin mutilar)
+// de las funciones declaradas en este cabezal.
+extern "C" {
+    #include "projectstalker/physics/manning_kernel.h"
+}
 #include <cuda_runtime.h>
 #include <iostream>
 #include <stdexcept>
